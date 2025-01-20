7da5184b03bff0d968ad8d0cf29ed0693edea999
ItaloFelipe <it4l0f5l1pe@gmail.com>
2025-01-20 18:09:02 -0300

    Added views to create/edit blog post inside the site.

--- appsite/views.py
+++ appsite/views.py
@@ -1,53 +1,42 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Post
 from .forms import PostForm
-from django.shortcuts import redirect
 
 def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'appsite/post_list.html', {'posts': posts})
-
-posts = Post.objects.filter(published_date__lte=timezone.now
-()).order_by('published_date')
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'appsite/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'appsite/post_edit.html', {'form': form})
 
-
-def post_new(request):
-     if request.method == "POST":
-         form = PostForm(request.POST)
-         if form.is_valid():
-             post = form.save(commit=False)
-             post.author = request.user
-             post.published_date = timezone.now()
-             post.save()
-             return redirect('post_detail', pk=post.pk)
-     else:
-         form = PostForm()
-     return render(request, 'appsite/post_edit.html', {'form': form})
-
+@login_required
 def post_edit(request, pk):
-     post = get_object_or_404(Post, pk=pk)
-     if request.method == "POST":
-         form = PostForm(request.POST, instance=post)
-         if form.is_valid():
-             post = form.save(commit=False)
-             post.author = request.user
-             post.published_date = timezone.now()
-             post.save()
-             return redirect('post_detail', pk=post.pk)
-     else:
-         form = PostForm(instance=post)
-     return render(request, 'appsite/post_edit.html', {'form': form})
-
-
-
-
-
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'appsite/post_edit.html', {'form': form})
